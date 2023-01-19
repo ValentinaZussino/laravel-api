edfb97af81ae10872e1aa6b05a6d8e6cc1ca4a73
ValentinaZussino <vzussino@protonmail.com>
2023-01-19 10:52:36 +0100

    config email

---
+++ resources/views/emails/new-contact-mail.blade.php
@@ -0,0 +1,5 @@
+<h1>Email</h1>
+<p>
+    Hai ricevuto un messaggio, ecco qui i dettagli:
+    
+</p>
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\LeadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::get('projects', [ProjectController::class, 'index']);
 Route::get('projects/{slug}', [ProjectController::class, 'show']);
+Route::post('/contacts', [LeadController::class, 'store']);
