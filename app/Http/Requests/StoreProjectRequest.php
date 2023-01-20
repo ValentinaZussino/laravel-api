895548de639702b4d49d743e21b934230a65cdb0
ValentinaZussino <vzussino@protonmail.com>
2023-01-20 18:12:59 +0100

    modifica in storeprojectrequest a difficulty

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -27,7 +27,7 @@ class StoreProjectRequest extends FormRequest
             'title' =>'required|unique:projects|max:100|min:3',
 	        'description' => 'nullable',
             'framework' => 'nullable',
-            'difficulty' => 'nullable',
+            'difficulty' => 'nullable|integer|between:1,10',
             'team' => 'nullable',
             'git_link' => 'nullable',
             'cover_image' => 'nullable|image|max:1000',
